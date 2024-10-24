a103af25b050569a583eb42e6a196962e39f6bca
kkkusto <147166214+kkkusto@users.noreply.github.com>
2024-10-24 05:59:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,83 +1,91 @@
-import os
-import datetime
 import subprocess
-import shutil
+import datetime
+import argparse
 
-# Define base archive path
-ARCHIVE_BASE_PATH = '/path/to/archive'
+BEELINE_CMD_TEMPLATE = "beeline -u '<jdbc_connection_string>' -e \"{}\""
 
-# Define the retention period (in years)
-RETENTION_YEARS = 11
 
-# Read table list from file
-def read_table_list(file_path):
+def execute_beeline(query):
+    command = BEELINE_CMD_TEMPLATE.format(query)
+    try:
+        subprocess.check_call(command, shell=True)
+    except subprocess.CalledProcessError as e:
+        print("Error executing beeline: {}".format(e))
+
+
+def get_table_location(table_name):
+    query = "DESCRIBE FORMATTED `{}`".format(table_name)
+    command = BEELINE_CMD_TEMPLATE.format(query)
+    output = subprocess.check_output(command, shell=True)
+    for line in output.splitlines():
+        if line.strip().startswith("Location"):
+            return line.split(":", 1)[-1].strip()
+    return None
+
+
+def get_table_partitions(table_name):
+    query = "SHOW PARTITIONS `{}`".format(table_name)
+    command = BEELINE_CMD_TEMPLATE.format(query)
+    output = subprocess.check_output(command, shell=True)
+    return output.splitlines()
+
+
+def archive_partition(table_name, partition, base_archive_path):
+    # Create directory for archived partition using HDFS commands
+    archive_dir = "{}/{}".format(base_archive_path, table_name)
+    subprocess.call(["hdfs", "dfs", "-mkdir", "-p", archive_dir])
+
+    # Archive partition to base archive path using HDFS commands
+    partition_path = get_table_location(table_name) + "/" + partition.replace("/", "")
+    subprocess.call(["hdfs", "dfs", "-cp", partition_path, archive_dir])
+
+    # Remove archived partition from the table
+    drop_query = "ALTER TABLE `{}` DROP PARTITION ({})".format(table_name, partition)
+    execute_beeline(drop_query)
+
+
+def main(file_path, base_archive_path):
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    min_year = datetime.datetime.now().year - 11
+
     with open(file_path, 'r') as f:
-        return [line.strip() for line in f if line.strip()]
+        for line in f:
+            table_name = line.strip()
+            if not table_name:
+                continue
 
-# Check the age of the partition using beeline
-# Assuming partition name is in 'YYYY-MM-DD' format
-def is_partition_older_than_beeline(table_name, partition_name, years):
-    # Run beeline command to get partition information
-    show_partition_command = (
-        "beeline -u 'jdbc:hive2://your_hive_server' "
-        "-e \"SHOW PARTITIONS {}\""
-        .format(table_name)
-    )
-    output = subprocess.check_output(show_partition_command, shell=True)
-    partitions = output.splitlines()
+            # Step 2: Get table location
+            table_location = get_table_location(table_name)
 
-    # Find partition and check its age
-    for partition in partitions:
-        if partition_name in partition:
-            try:
-                partition_date = datetime.datetime.strptime(partition_name, '%Y-%m-%d')
-            except ValueError:
-                # If partition_name does not match the expected format, skip it
-                return False
+            # Step 3: Insert operation into archival_operation table
+            insert_query = "INSERT INTO TABLE archival_operation VALUES ('{}', '{}', '{}', '{}')".format(table_name, table_location, "operation_id_placeholder", current_date)
+            execute_beeline(insert_query)
 
-            cutoff_date = datetime.datetime.now() - datetime.timedelta(days=years * 365)
-            return partition_date < cutoff_date
-    return False
+            # Step 4-6: Get partitions and identify partitions older than 11 years
+            partitions = get_table_partitions(table_name)
+            for partition in partitions:
+                partition_values = partition.split('/')
+                partition_year_str = None
+                for value in partition_values:
+                    if "=" in value:
+                        key, val = value.split("=")
+                        if len(val) == 4 and val.isdigit():
+                            partition_year_str = val
+                            break
 
-# Create archive directory if it doesn't exist
-def create_archive_directory(archive_path):
-    if not os.path.exists(archive_path):
-        os.makedirs(archive_path)
+                if partition_year_str and int(partition_year_str) <= min_year:
+                    # Step 7: Insert into table_partitions_archival
+                    insert_partition_query = "INSERT INTO TABLE table_partitions_archival VALUES ('{}', '{}')".format(table_name, partition)
+                    execute_beeline(insert_partition_query)
 
-# Move partition to archive and remove from table using beeline
-def archive_partition(table_name, partition_name):
-    archive_path = os.path.join(ARCHIVE_BASE_PATH, table_name)
-    create_archive_directory(archive_path)
-    print('Archiving partition: {}'.format(partition_name))
-    
-    # Run beeline command to alter table and drop partition
-    drop_partition_command = (
-        "beeline -u 'jdbc:hive2://your_hive_server' "
-        "-e \"ALTER TABLE {} DROP IF EXISTS PARTITION (date='{}')\""
-        .format(table_name, partition_name)
-    )
-    subprocess.call(drop_partition_command, shell=True)
+                    # Step 8-9: Archive partition and delete it
+                    archive_partition(table_name, partition, base_archive_path)
 
-    # Move the partition directory to the archive location
-    partition_path = os.path.join('/path/to/tables', table_name, partition_name)
-    shutil.move(partition_path, archive_path)
 
-# Main function to iterate over tables and archive partitions
-def archive_old_partitions(table_list_file):
-    table_list = read_table_list(table_list_file)
-    for table_name in table_list:
-        table_path = os.path.join('/path/to/tables', table_name)
-        if not os.path.exists(table_path):
-            print('Table path not found: {}'.format(table_path))
-            continue
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Archive old Hadoop table partitions.')
+    parser.add_argument('file_path', help='Path to the file containing Hadoop table names')
+    parser.add_argument('base_archive_path', help='Base HDFS path for archiving partitions')
+    args = parser.parse_args()
 
-        # List partitions in table directory
-        partitions = [d for d in os.listdir(table_path) if os.path.isdir(os.path.join(table_path, d))]
-        for partition_name in partitions:
-            if is_partition_older_than_beeline(table_name, partition_name, RETENTION_YEARS):
-                archive_partition(table_name, partition_name)
-
-if __name__ == '__main__':
-    # Provide the path to the file containing the list of tables
-    table_list_file = '/path/to/table_list.txt'
-    archive_old_partitions(table_list_file)
+    main(args.file_path, args.base_archive_path)
